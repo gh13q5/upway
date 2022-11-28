be9141707779d55a668f8b3ecb10d9aaa05c19b8
gs25q <rs13q5@gmail.com>
2022-11-28 17:22:32 +0900

    추가 : Menu/User/User_scrap 도메인 추가

---
+++ app/src/main/java/com/mobile/upway/Menu.java
@@ -0,0 +1,49 @@
+package com.mobile.upway;
+
+public class Menu {
+    int id;
+    String name;
+    int kcal;
+    int price;
+    int size;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(int kcal) {
+        this.kcal = kcal;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
---
+++ app/src/main/java/com/mobile/upway/User_scrap.java
@@ -0,0 +1,24 @@
+package com.mobile.upway;
+
+public class User_scrap {
+    User user;
+    //Combination comb;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    /*
+    public Combination getComb() {
+        return comb;
+    }
+
+    public void setComb(Combination comb) {
+        this.comb = comb;
+    }
+
+     */
+}
