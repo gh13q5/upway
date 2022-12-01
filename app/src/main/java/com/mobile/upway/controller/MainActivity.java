b94a731feecc22bc3945a3ffcad0a889193e239a
gs25q <rs13q5@gmail.com>
2022-12-01 17:32:59 +0900

    수정 : DAO 파일 수정

--- app/src/main/java/com/mobile/upway/controller/MainActivity.java
+++ app/src/main/java/com/mobile/upway/controller/MainActivity.java
@@ -1,10 +1,17 @@
 package com.mobile.upway.controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mobile.upway.R;
+import com.mobile.upway.dto.User;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/mobile/upway/dao/UserScrapDAO.java
+++ app/src/main/java/com/mobile/upway/dao/UserScrapDAO.java
@@ -5,30 +5,33 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.mobile.upway.dto.User_scrap;
 
 public class UserScrapDAO {
-    public static String TAG = "UserDAO";
+    public static String TAG = "UserScrapDAO";
 
     private User_scrap userScrap;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    db.collection("UserScrap")
-            .get()
-        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-        @Override
-        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Log.d(TAG, document.getId() + " => " + document.getData());
-                }
-            } else {
-                Log.w(TAG, "Error getting documents.", task.getException());
-            }
-        }
-    });
+    public void readUserScrap() {
+        db.collection("UserScrap")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
 
 }
