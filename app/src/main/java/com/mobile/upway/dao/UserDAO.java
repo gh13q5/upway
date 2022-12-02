b28adcc153a4f1d97a71887e9517336ac00b4fc6
gs25q <rs13q5@gmail.com>
2022-12-02 20:22:41 +0900

    수정 : DAO 파일 수정

--- app/src/main/java/com/mobile/upway/dao/UserDAO.java
+++ app/src/main/java/com/mobile/upway/dao/UserDAO.java
@@ -7,34 +7,61 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.mobile.upway.dto.Cheese;
 import com.mobile.upway.dto.User;
+
+import java.util.Map;
 
 public class UserDAO {
     public static String TAG = "UserDAO";
 
-    private User user;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db;
+    private CollectionReference userColl;
 
-    public void readUser() {
-        db.collection("User")
+    public UserDAO() {
+        db = FirebaseFirestore.getInstance();
+        userColl = db.collection("user");
+    }
+
+    public User findUserById(String userId) {
+        User[] user = {null};
+        userColl.whereEqualTo("userId", userId)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                user[0] = new User();
+                                Map<String, Object> data = document.getData();
+                                for (Map.Entry<String, Object> entry : data.entrySet()) {
+                                    switch (entry.getKey()) {
+                                        case "userId":
+                                            user[0].setUserId(entry.getValue().toString());
+                                            break;
+                                        case "password":
+                                            user[0].setPassword(entry.getValue().toString());
+                                            break;
+                                        case "nickname":
+                                            user[0].setNickname(entry.getValue().toString());
+                                            break;
+                                    }
+                                }
                             }
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Log.d(TAG, "No such user");
                         }
                     }
                 });
+        return user[0];
     }
-
 }
--- app/src/main/java/com/mobile/upway/dao/UserScrapDAO.java
+++ app/src/main/java/com/mobile/upway/dao/UserScrapDAO.java
@@ -6,32 +6,21 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.mobile.upway.dto.User;
 import com.mobile.upway.dto.User_scrap;
 
+import java.util.Map;
+
 public class UserScrapDAO {
-    public static String TAG = "UserScrapDAO";
+    private FirebaseFirestore db;
+    private CollectionReference userScrapColl;
 
-    private User_scrap userScrap;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    public void readUserScrap() {
-        db.collection("UserScrap")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+    public UserScrapDAO() {
+        db = FirebaseFirestore.getInstance();
+        userScrapColl = db.collection("userScrap");
     }
-
 }
