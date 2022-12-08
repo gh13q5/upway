499d9883b5e913b855748105ab734c5d6ca5f5c7
soobnini <soobiny4339@gmail.com>
2022-12-08 22:17:49 +0900

    DAO추가/DTO수정

---
+++ app/src/main/java/com/mobile/upway/controller/DetailActivity.java
@@ -0,0 +1,4 @@
+package com.mobile.upway.controller;
+
+public class DetailActivity {
+}
--- app/src/main/java/com/mobile/upway/dto/Combination.java
+++ app/src/main/java/com/mobile/upway/dto/Combination.java
@@ -1,31 +1,37 @@
 package com.mobile.upway.dto;
 
+import java.util.List;
+
 public class Combination {
-    public int combId;
     public String title;
     public String description;
     public int scraps;
     public int kcal;
     public int price;
+    public String user;
+    public String menu;
+    public String bread;
+    public String cheese;
+    public List<String> vegetableList;
+    public List<String> sauceList;
+    public List<String> optionsList;
 
     public Combination() {
     }
 
-    public Combination(int combId, String title, String description, int scraps, int kcal, int price) {
-        this.combId = combId;
+    public Combination(String title, String description, int scraps, int kcal, int price, String user, String menu, String bread, String cheese, List<String> vegetableList, List<String> sauceList, List<String> optionsList) {
         this.title = title;
         this.description = description;
         this.scraps = scraps;
         this.kcal = kcal;
         this.price = price;
-    }
-
-    public int getCombId() {
-        return combId;
-    }
-
-    public void setCombId(int combId) {
-        this.combId = combId;
+        this.user = user;
+        this.menu = menu;
+        this.bread = bread;
+        this.cheese = cheese;
+        this.vegetableList = vegetableList;
+        this.sauceList = sauceList;
+        this.optionsList = optionsList;
     }
 
     public String getTitle() {
@@ -66,5 +72,61 @@ public class Combination {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public String getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(String cheese) {
+        this.cheese = cheese;
+    }
+
+    public List<String> getVegetableList() {
+        return vegetableList;
+    }
+
+    public void setVegetableList(List<String> vegetableList) {
+        this.vegetableList = vegetableList;
+    }
+
+    public List<String> getSauceList() {
+        return sauceList;
+    }
+
+    public void setSauceList(List<String> sauceList) {
+        this.sauceList = sauceList;
+    }
+
+    public List<String> getOptionsList() {
+        return optionsList;
+    }
+
+    public void setOptionsList(List<String> optionsList) {
+        this.optionsList = optionsList;
     }
 }
