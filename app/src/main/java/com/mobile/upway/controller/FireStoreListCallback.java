1c9cc46d164ccf2d13202fa61e2fc9f37d49388a
soobnini <soobiny4339@gmail.com>
2022-12-17 23:26:19 +0900

    CallBack Interface 수정

--- app/src/main/java/com/mobile/upway/controller/FireStoreListCallback.java
+++ app/src/main/java/com/mobile/upway/controller/FireStoreListCallback.java
@@ -1,9 +1,10 @@
 package com.mobile.upway.controller;
 
 import com.mobile.upway.dto.Combination;
+import com.mobile.upway.dto.Comment;
 
 import java.util.List;
 
-public interface FireStoreListCallback {
-    void onCallbackCombList(List<Combination> combList);
+public interface FireStoreListCallback<E> {
+    void onCallbackList(List<E> combList);
 }
